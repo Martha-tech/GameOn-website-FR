0c0addf8fdf37bd443870e4732f247b426b1fcd2
Martha-tech <martha.makassikis@gmail.com>
2021-09-22 01:10:22 +0200

    #1 fermeture de la modale

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,11 +1,21 @@
-function editNav() {
+var editNav = function() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
     x.className += " responsive";
   } else {
     x.className = "topnav";
   }
+
 }
+
+var close_btn = document.getElementById("btn-close");
+var on_click_close = function(e) {
+  var f = document.getElementById("div-bground");
+  f.style.display = "none";
+}
+close_btn.addEventListener("click", on_click_close);
+
+
 
 // DOM Elements
 const modalbg = document.querySelector(".bground");
