ad6bb849166f0601cb98f88ed282f603ff69f51a
Martha-tech <martha.makassikis@gmail.com>
2021-09-22 16:33:25 +0200

    debut #2

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -8,13 +8,43 @@ var editNav = function() {
 
 }
 
+// Fermeture de la modale #1
 var close_btn = document.getElementById("btn-close");
-var on_click_close = function(e) {
-  var f = document.getElementById("div-bground");
-  f.style.display = "none";
+var close_modale = function(e) {
+  var modale = document.getElementById("div-bground");
+  modale.style.display = "none";
 }
-close_btn.addEventListener("click", on_click_close);
+close_btn.addEventListener("click", close_modale);
 
+// Implémentation des entrées du formulaire #2
+
+var nom = document.getElementById("last");
+var courriel = document.getElementById("email");
+var date_naissance = document.getElementById("birthdate");
+var quantite = document.getElementById("quantity");
+var location = document.querySelectorAll('input[name="location"]');
+var submit_form = document.getElementsByClassName("btn-submit");
+
+
+/*var prenom_valide = function(e) {
+  var prenom = document.getElementById("first");
+  if (prenom.length >= 2) {
+    return true;
+  } else {
+    window.alert("Veuillez entrer 2 caractères ou plus pour le champ du prénom.");
+    return false;
+  };
+}
+prenom.addEventListener("cli???ck", prenom_valide);
+
+var nom_valide = function(e) {
+  if (e.length >= 2) {
+    return true;
+  } else {
+    window.alert("Veuillez entrer 2 caractères ou plus pour le champ du nom.");
+    return false;
+  };
+}*/
 
 
 // DOM Elements
