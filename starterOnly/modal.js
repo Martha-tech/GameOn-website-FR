ea4224ca2435aab627d72d46a51cd29be68d9c86
Martha-tech <martha.makassikis@gmail.com>
2021-10-20 08:06:05 +0200

    code #2 (boutons radio) & correction des erreurs d'affichage #5
    Il reste encore quelques erreurs à corriger (cf. affichage de la topnav dans la modale en version mobile).

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -16,39 +16,20 @@ var close_modale = function(e) {
 }
 close_btn.addEventListener("click", close_modale);
 
-// Implémentation des entrées du formulaire #2 et ajout d'une validation ou de messages d'erreurs #3
+// Implémentation des entrées du formulaire #2 
+// Ajout d'une validation ou de messages d'erreurs #3
 /*
 LORSQU'un champ du formulaire n'est pas bien rempli,
   faire apparaître la div <error_...> sous le champ.
-   -> display: block ou inline-block
+   -> display: block
 
 LORSQUE l'ensemble des champs du formulaire ont bien été remplis,
   faire apparaître la div <confirmation_envoi>
   et faire disparaître la div <modal-body>.
     -> display: none
 */
-
-/*var firstname = document.getElementById("first");
-var lastname = document.getElementById("last");
-var emailAddress = document.getElementById("email");
-var birthDate = document.getElementById("birthdate");
-var tournamentsQuantity = document.getElementById("quantity");
-var town = document.querySelectorAll('input[name="location"]');
-var conditions = document.getElementById("checkbox1");
-var form_submit = document.getElementById("submit1");
-var confirm_submit = document.getElementById("confirmation_envoi");
-var formulaire = document.getElementById("form");
-
-var error_firstname = document.querySelector(".error_first");
-var error_lastname = document.querySelector(".error_last");
-var error_emailAddress = document.querySelector(".error_email");
-var error_birthdate = document.querySelector(".error_birthdate");
-var error_tournamentsQuantity = document.querySelector(".error_quantity");
-var error_town = document.querySelector(".error_town");
-var error_conditions = document.querySelector(".error_conditions");
-*/
  
-//Vérification du formulaire
+// Vérification du formulaire
 function form_check() {
 
   //prenom
@@ -102,15 +83,15 @@ function form_check() {
   //date de naissance
   function ValidateBirthdate(birthdate) {
     console.log(birthdate);
-    var error_birthdate = document.querySelector(".error_birthdate");
-    var regexVar = /^[0-9]{4}-[0-9]{2}-[0-9]{2}$/;
-    var regexVarTest = regexVar.test(birthdate);
-    var userBirthDate = new Date(birthdate);
+    let error_birthdate = document.querySelector(".error_birthdate");
+    let regexVar = /^[0-9]{4}-[0-9]{2}-[0-9]{2}$/;
+    let regexVarTest = regexVar.test(birthdate);
+    let userBirthDate = new Date(birthdate);
 
-    var todayYear = (new Date()).getFullYear();
-    var cutOff18 = new Date();
+    let todayYear = (new Date()).getFullYear();
+    let cutOff18 = new Date();
     cutOff18.setFullYear(todayYear - 18); 
-    var result = false;
+    let result = false;
 
     if (!regexVarTest) {
       error_birthdate.innerHTML = "Veuillez entrer votre date de naissance au format jj/mm/aaaa.";
@@ -138,13 +119,12 @@ function form_check() {
   }
 
   //nombre de tournois
-
   function ValidateQuantity(quantity) {
     let toNumber = parseInt(quantity, 10);
     console.log(typeof quantity);
 
-    var error_tournamentsQuantity = document.querySelector(".error_quantity");
-    var result = false;
+    let error_tournamentsQuantity = document.querySelector(".error_quantity");
+    let result = false;
 
     if (isNaN(toNumber) || toNumber % 1 != 0 || quantity == null) {
       error_tournamentsQuantity.style.display = "block";
@@ -155,9 +135,8 @@ function form_check() {
     return result;
   }
 
-  var tournamentsQuantity = document.getElementById("quantity");
-  var error_tournamentsQuantity = document.querySelector(".error_quantity");
-  var inputError = document.querySelector(".input_error");
+  let tournamentsQuantity = document.getElementById("quantity");
+  let error_tournamentsQuantity = document.querySelector(".error_quantity");
 
   if (ValidateQuantity(tournamentsQuantity.value)) {
     console.log("Number is valid.");
@@ -165,7 +144,30 @@ function form_check() {
   } else {
     console.log("Number is invalid.");
     error_tournamentsQuantity.style.display = "block";
-    inputError.style.border = "0.8px solid red"; /*ne fonctionne pas*/
+    valeur = false;
+  }
+
+  //villes
+  const towns = document.querySelectorAll('input[name="location"]');
+  let error_town = document.querySelector(".error_town");
+
+  function townCheck (t) {
+    let result = true;
+    for (var i = 0; i < t.length; i++) {
+      if (t[i].checked) {
+        result = true;
+        break;
+      } else {
+        result = false;  
+      }
+    }
+    return result;
+  }
+ 
+  if (townCheck(towns) == true) {
+    error_town.style.display = "none";
+  } else {
+    error_town.style.display = "block";
     valeur = false;
   }
 
@@ -182,36 +184,35 @@ function form_check() {
     error_conditions.style.display = "block";
     valeur = false;
   }
-
+  console.log(valeur);
   return valeur;
 }
 
-//Soumission du formulaire et validation
+//Soumission du formulaire et validation #4
 
-var form_submit = document.querySelector(".btn-submit");
+let form_submit = document.querySelector(".btn-submit");
 form_submit.addEventListener("click", form_validate);
 
 function form_validate(e) {
-  var confirm_submit = document.getElementById("confirm_submit");
+  let confirm_submit = document.getElementById("confirm_submit");
   e.preventDefault();
 
   if (form_check() == true) {
-    var formulaire = document.querySelector("form");
+    let formulaire = document.querySelector("form");
     confirm_submit.style.display = "block";
     formulaire.style.display = "none";
 
-    var fermer_btn = document.getElementById("submit2")
+   let fermer_btn = document.getElementById("submit2")
     fermer_btn.style.display = "block";
 
     fermer_btn.addEventListener("click", close_modale);
   }
 }
 
-//Bouton "c'est parti" déclenchant l'apparition de la div "confirmation d'envoi (de l'inscription)"
 
 if (form.click == true) {
   console.log("Registration form has been sent.");
-  var confirm_submit = document.getElementById("confirm_submit");
+  //let confirm_submit = document.getElementById("confirm_submit");
 }
 
 // DOM Elements
